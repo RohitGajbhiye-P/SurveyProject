d415e28763c88f8de545f02179b22e32c0b09a75
Rohit <shivang.son786@gmail.com>
2020-09-26 10:14:19 +0530

    Add project files.

---
+++ SurveyProject/Controllers/DataController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SurveyProject.Models;
+
+namespace SurveyProject.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly ConnectionModel _context;
+
+        public DataController(ConnectionModel context)
+        {
+            _context = context;
+        }
+
+        // GET: Data
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DataModel.ToListAsync());
+        }
+
+        // GET: Data/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dataModel = await _context.DataModel
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (dataModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(dataModel);
+        }
+
+        // GET: Data/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Data/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,AccountNumber,BenificiaryName,BankName,SWIFTCode,Amount")] DataModel dataModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dataModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dataModel);
+        }
+
+        // GET: Data/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dataModel = await _context.DataModel.FindAsync(id);
+            if (dataModel == null)
+            {
+                return NotFound();
+            }
+            return View(dataModel);
+        }
+
+        // POST: Data/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,AccountNumber,BenificiaryName,BankName,SWIFTCode,Amount")] DataModel dataModel)
+        {
+            if (id != dataModel.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dataModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DataModelExists(dataModel.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dataModel);
+        }
+
+        // GET: Data/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dataModel = await _context.DataModel
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (dataModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(dataModel);
+        }
+
+        // POST: Data/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dataModel = await _context.DataModel.FindAsync(id);
+            _context.DataModel.Remove(dataModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DataModelExists(int id)
+        {
+            return _context.DataModel.Any(e => e.id == id);
+        }
+    }
+}
---
+++ SurveyProject/Models/DataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyProject.Models
+{
+    public class DataModel
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string BenificiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+        public string SWIFTCode { get; set; }
+        public int Amount { get; set; }
+    }
+}
